0bc99a3f34b67ad4af9cbae5b471374945915250
curloz123 <curloz@tutamail.com>
2026-03-30 15:43:50 +0530

    ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://curl0z.codeberg.page',
-	base: '/vklearn-site',
+	// base: '/vklearn-site',
 	integrations: [
 		starlight({
 			title: 'vklearn',
